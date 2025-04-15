4aa77aeda46ca9d6f69a49972c4044892dc9756f
yadavkajal <kajalyadav459@gmail.com>
2025-04-16 01:22:55 +0530

    json

--- json_assignment.py
+++ json_assignment.py
@@ -5,6 +5,5 @@ with open("subject_details.json","r") as file:
 print("Sub | Sem | Marks | Pass")
 def func(json_data):
     for key in json_data:
-    
         print(key, "|",json_data[key].get("semester"),"|",json_data[key].get("marks"),"|",json_data[key].get("isPass"))
 func(json_data)
